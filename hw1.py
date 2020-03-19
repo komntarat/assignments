ee3483b0453a3f72c0389e01850e463318b245b2
komntarat <62241923+komntarat@users.noreply.github.com>
2020-03-19 12:32:50 +0100

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -27,9 +27,12 @@ def poland_cases_by_date(day: int, month: int, year: int = 2020) -> int:
     :param month: Month to get the cases for as an integer indexed from 1
     :return: Number of cases on a given date as an integer
     """
-    
-    # Your code goes here (remove pass)
-    pass
+   def poland_cases_by_date(day: int, month: int, year: int=2020):
+            data=f"{month}/{day}/20"
+            url = f"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+            df = pd.read_csv(url, error_bad_lines=False)
+            result = df.loc[df["Country/Region"]=="Poland"][data].values[0]
+            return result
 
 
 def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
@@ -47,9 +50,9 @@ def top5_countries_by_date(day: int, month: int, year: int = 2020) -> List[str]:
     :param year: Month to get the countries for as an integer indexed from 1
     :return: A list of strings with the names of the coutires
     """
-
-    # Your code goes here (remove pass)
-    pass
+    def top5_countries_by_date(day: int, month: int, year: int=2020):
+    data=f"{month}/{day}/20"
+    return df[["Province/State","Country/Region", data]].sort_values(by=data).tail(5).sort_values(by=data, ascending=False)
 
 # Function name is wrong, read the pydoc
 def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
@@ -68,6 +71,3 @@ def no_new_cases_count(day: int, month: int, year: int = 2020) -> int:
     :param year: Month to get the countries for as an integer indexed from 1
     :return: Number of countries/regions where the count has not changed in a day
     """
-    
-    # Your code goes here (remove pass)
-    pass
